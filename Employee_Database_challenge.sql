1101da34005a3cc2bdc5bdd0c3fa7e2114eb79bb
Solrys <74037270+Solrys@users.noreply.github.com>
2020-12-31 11:57:31 -0800

    Challenge

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,3 +1,5 @@
+
+-- 1. QUERY FOR RETIREMENT TITLES
 SELECT e.emp_no,
     e.first_name, e.last_name, t.title, t.from_date, t.to_date
 INTO title_retirement
@@ -5,4 +7,36 @@ FROM employees as e
 LEFT JOIN titles as t
 ON e.emp_no = t.emp_no
 
+--CHECK TABLE BEFORE EXPORTING TO retirement_titles.csv.
 SELECT * FROM title_retirement
+
+
+
+
+ -- 2. QUERY FOR UNIQUE TITLES
+SELECT DISTINCT ON (emp_no) emp_no, 
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+--CHECK TABLE BEFORE EXPORTING TO unique_titles.csv
+SELECT * FROM unique_titles
+
+
+
+
+-- 3. QUERY FOR RETIREMNT TITLE COUNT DESCENDING
+
+SELECT COUNT (title), title
+INTO count_titles
+from unique_titles
+group by title
+ORDER by count DESC;
+
+
+--CHECK TABLE BEFORE EXPORTING TO retiring_titles.csv.
+SELECT * FROM count_titles
+
